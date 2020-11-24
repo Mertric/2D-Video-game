e026346c6a379975a31f3dbe9ee1c35b1c2ec79d
Tim <Pasiontimothy@gmail.com>
2020-11-24 04:36:32 -0600

    Recent Change To Movement Scripts

--- 2D Platformer/src/Actors/Actor.gd
+++ 2D Platformer/src/Actors/Actor.gd
@@ -1,14 +1,16 @@
 extends KinematicBody2D
 class_name Actor
 
+#var FLOOR_NORMAL = Vector2.UP
+const FLOOR_NORMAL: = Vector2.UP
 export var gravity: = 3000.0
 export var speed: = Vector2(300.0, 1000.0)
-
 var velocity: = Vector2.ZERO
 
 #this physics process will excute before the children classes
 #this function will allow the player to move
-func _physics_process(delta: float) -> void:
-	velocity.y	+= gravity * delta
-	#velocity.y = max(velocity.y, speed.y)
-	velocity = move_and_slide(velocity)
+
+#func _physics_process(delta: float) -> void:
+#	velocity.y	+= gravity * delta
+#	#velocity.y = max(velocity.y, speed.y)
+#	velocity = move_and_slide(velocity)
--- 2D Platformer/src/Actors/Player.gd
+++ 2D Platformer/src/Actors/Player.gd
@@ -1,15 +1,27 @@
 extends Actor
 
+var direction: = Vector2.ZERO
+
 #built in Godot Function
+#delta is a timer
 func _physics_process(delta: float) -> void:
 	#calculate the direction the character is trying to move at every frame
-	
 	#returns a floating point value return 1 if pressed 0 if not
 	var direction: = GetDirection() 
-	velocity = speed * direction
+	velocity = CalculateMoveVelocity(velocity,direction,speed)
+	velocity = move_and_slide(velocity, FLOOR_NORMAL	)
 
 #This function will get the direction of the player when key is pressed for either A=Left, D=Right, or SpaceBar=Jump
 #Jumping will only occur if the play is on the floor.
 func GetDirection() -> Vector2:
 	return Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"),-1.0
 	if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0)	
+
+#This will calculate the player movement velocity
+func CalculateMoveVelocity(linearVelocity: Vector2,speed: Vector2, direction: Vector2) -> Vector2:
+	var newVelocity: = linearVelocity
+	newVelocity.x = speed.x * direction.x
+	newVelocity.y += gravity * get_physics_process_delta_time()
+	if direction.y == -1.0:
+		newVelocity.y = speed.y * direction.y
+	return  newVelocity
