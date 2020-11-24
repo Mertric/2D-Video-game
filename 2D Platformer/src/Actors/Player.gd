b0f5d43fb4c9b160a4b6fff9ef59ec154dc3721f
Tim <Pasiontimothy@gmail.com>
2020-11-24 14:07:17 -0600

    Finished Character Movement
    The character is able to go left, right and jump a specific height when the user holds the spacebar.

--- 2D Platformer/src/Actors/Player.gd
+++ 2D Platformer/src/Actors/Player.gd
@@ -1,15 +1,15 @@
 extends Actor
 
 var direction: = Vector2.ZERO
-
 #built in Godot Function
 #delta is a timer
 func _physics_process(delta: float) -> void:
+	var isJumpInterrupted: = Input.is_action_just_released("jump") and velocity.y < 0.0
 	#calculate the direction the character is trying to move at every frame
 	#returns a floating point value return 1 if pressed 0 if not
 	var direction: = GetDirection() 
-	velocity = CalculateMoveVelocity(velocity,direction,speed)
-	velocity = move_and_slide(velocity, FLOOR_NORMAL	)
+	velocity = CalculateMoveVelocity(velocity,speed,direction,isJumpInterrupted)
+	velocity = move_and_slide(velocity, FLOOR_NORMAL)
 
 #This function will get the direction of the player when key is pressed for either A=Left, D=Right, or SpaceBar=Jump
 #Jumping will only occur if the play is on the floor.
@@ -18,10 +18,12 @@ func GetDirection() -> Vector2:
 	if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0)	
 
 #This will calculate the player movement velocity
-func CalculateMoveVelocity(linearVelocity: Vector2,speed: Vector2, direction: Vector2) -> Vector2:
+func CalculateMoveVelocity(linearVelocity: Vector2,speed: Vector2, direction: Vector2, jumpInterrtuped: bool) -> Vector2:
 	var newVelocity: = linearVelocity
 	newVelocity.x = speed.x * direction.x
 	newVelocity.y += gravity * get_physics_process_delta_time()
 	if direction.y == -1.0:
 		newVelocity.y = speed.y * direction.y
+	if jumpInterrtuped:
+		newVelocity.y = 0.0
 	return  newVelocity
