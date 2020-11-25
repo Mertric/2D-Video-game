3effe886566ee0beda0d0d361a5dd2c39982ed26
Tim <Pasiontimothy@gmail.com>
2020-11-25 02:42:54 -0600

    Added Camera to the Player
    Added camera to player to see how the game will look, as well as coded the monster to only start when the player enters a certain view length. The monster will leave the view and not reappear once the player has moved past a certain distance from the monster.

--- 2D Platformer/src/Actors/Monster.gd
+++ 2D Platformer/src/Actors/Monster.gd
@@ -1,9 +1,15 @@
 extends "res://src/Actors/Actor.gd"
 
+#the visabilityEnabler2D will disable the monster from moving when it is out of the frame of the player.
+
+#ready function
 #this is called by every node by every scene starting at the deepest node of the scene tree.
 func _ready():
+	#this will deactivate the enemy so it will never enter the screen
+	set_physics_process(false)
 	velocity.x = -speed.x
 
+#This function wil allow the monster to move along the x axis 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 	if  is_on_wall():
