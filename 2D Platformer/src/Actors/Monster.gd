973cf197ffb41e02ffc9501610326daff0a29cb0
Tim <Pasiontimothy@gmail.com>
2020-11-26 14:46:40 -0600

    feat: added animations to Player

--- 2D Platformer/src/Actors/Monster.gd
+++ 2D Platformer/src/Actors/Monster.gd
@@ -13,12 +13,13 @@ func _ready():
 func _on_JumpDetector_body_entered(body: PhysicsBody2D) -> void:
 	if body.global_position.y > get_node("JumpDetector").global_position.y:
 		return
-	get_node("CollisionShape2D").disabled = true
+	$CollisionShape2D.set_deferred("disabled", true) 
 	queue_free()
 
-#This function wil allow the monster to move along the x axis 
+#This function wil allow the monster to move along the axis 
 func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
+	#flip make the monster go the other direction on contact of the wall 
 	if  is_on_wall():
 		velocity.x *= -1.0
 	velocity.y = move_and_slide(velocity, FLOOR_NORMAL).y
--- 2D Platformer/src/Actors/Player.gd
+++ 2D Platformer/src/Actors/Player.gd
@@ -1,32 +1,36 @@
-#extends Actor
-extends "res://src/Actors/Actor.gd"
+extends Actor
 
-#: = Vector2.ZERO
+#var direction: = Vector2.ZERO
 export var stompImpulse: = 1000.0
 
 var facing_right = true
 #built in Godot Function
 #delta is a timer
+
 func _on_EnemyDetector_area_entered(area: Area2D) -> void:
+	$AnimatedSprite.play("Jump")
 	velocity = CalculateStompVelocity(velocity, stompImpulse)
 
-func _on_EnemyDetector_body_entered(body: PhysicsBody2D) -> void:
+#if the enemy kinematic body enters this area the player will die
+func _on_EnemyDetector_body_entered(body:PhysicsBody2D) -> void:
 	queue_free()
+
 
 func _physics_process(delta: float) -> void:
 	var isJumpInterrupted: = Input.is_action_just_released("jump") and velocity.y < 0.0
 	#calculate the direction the character is trying to move at every frame
 	#returns a floating point value return 1 if pressed 0 if not
-	var direction: = GetDirection()
+	AnimSprite() 
+	var direction: = GetDirection() 
 	velocity = CalculateMoveVelocity(velocity,speed,direction,isJumpInterrupted)
 	velocity = move_and_slide(velocity, FLOOR_NORMAL)
-
+	
 
 #This function will get the direction of the player when key is pressed for either A=Left, D=Right, or SpaceBar=Jump
 #Jumping will only occur if the play is on the floor.
 func GetDirection() -> Vector2:
 	return Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"),-1.0
-	if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0)
+	if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0)	
 
 #This will calculate the player movement velocity
 func CalculateMoveVelocity(linearVelocity: Vector2,speed: Vector2, direction: Vector2, jumpInterrtuped: bool) -> Vector2:
@@ -38,22 +42,48 @@ func CalculateMoveVelocity(linearVelocity: Vector2,speed: Vector2, direction: Ve
 	if jumpInterrtuped:
 		outputVelocity.y = 0.0
 	return  outputVelocity
-
-
+	
+	
 #This function will replace the y component of the linear velocity vector with the impulse, this will launch the player
-#upwards
+#upwards 
 func CalculateStompVelocity(linearVelocity: Vector2, impulse: float) -> Vector2:
 	var output: = linearVelocity
 	output.y = -impulse
 	return output
 
+#Animation of the sprite.
+#when the  user presses the input buttons the sprite will move accordingly  
+func AnimSprite() -> void:
+	if facing_right == true:
+		$AnimatedSprite.scale.x = 1
+	else:
+		$AnimatedSprite.scale.x = -1
+		
+	if Input.is_action_pressed("right"):
+		facing_right = true
+		$AnimatedSprite.play("Run")
+	elif Input.is_action_pressed("left"):
+		facing_right = false
+		$AnimatedSprite.play("Run")
+	else:
+		$AnimatedSprite.play("Idle")
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			$AnimatedSprite.play("Jump")
+	
 
-#when player contact
-#collision detection?
-#func onHit(enemy: Monster)-> void:
-#	var
 
-func hitPointCheking(omni: int)-> void :
-	var player_v = get_node("/root/Player")
-	if player_v.hitPoint <= 0:
-		#on screen: dead
+
+
+
+
+
+
+
+
+
+
+
+
+
