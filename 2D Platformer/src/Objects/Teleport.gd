7374d4e4b3a6e4d52761b6f7261a8b34e67d6050
Tim <Pasiontimothy@gmail.com>
2020-11-27 01:03:11 -0600

    feat: Added logic to objective and teleport to next scene
    The portal and objective are now usable.

---
+++ 2D Platformer/src/Objects/Teleport.gd
@@ -0,0 +1,23 @@
+tool
+extends Area2D
+
+onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+#this is the type of tscn file
+export var nextScene: PackedScene
+
+func _on_Area2D_body_entered(body:PhysicsBody2D) -> void:
+	Teleport()
+	
+func _get_configuration_warning() -> String:
+	return "The next Scene property can't be empty" if not nextScene else "" 
+
+
+func Teleport() -> void:
+	animationPlayer.play("Fade_In")
+	yield(animationPlayer, "animation_finished")
+	#change to next scene
+	get_tree().change_scene_to(nextScene)
+
+
+
+
