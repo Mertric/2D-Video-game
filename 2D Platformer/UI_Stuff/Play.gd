56693dbd1719386aba4de6b51b16f6aa5db53d26
Tim <Pasiontimothy@gmail.com>
2020-11-30 02:56:04 -0600

    chore:updated play again
    Fixed Play again button as well as restarting the player score to zero when going to the main menu and playing again.

--- 2D Platformer/UI_Stuff/Play.gd
+++ 2D Platformer/UI_Stuff/Play.gd
@@ -5,6 +5,8 @@ export(String, FILE) var nextScenePath: = ""
 
 func _on_Play_button_up():
 	get_tree().change_scene(nextScenePath)
+	get_tree().paused = false
+	PlayerData.score = 0
 
 func _get_configuration_warning() -> String:
 	return "Need file path to next scene for play button to work" if nextScenePath == "" else ""
